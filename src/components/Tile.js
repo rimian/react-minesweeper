6f50cfdd3e8a0abbf2c33bb7d1c2e38820af3146
Rimian Perkins <hello@rimian.com.au>
2019-08-09 17:11:23 +1000

    one thing at a time! rm redux

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,19 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-import { connect } from 'react-redux';
-import { onRevealTile } from '../lib/redux';
-
-export default function Tile({ tile: { id, state }, onRevealTile }) {
+export default function Tile() {
   return (
-    <div className={`tile ${state}`} onClick={() => onRevealTile(id)}></div>
+    <div className={`tile DEFAULT`}></div>
   );
-};
-
-Tile.propTypes = {
-  tile: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    state: PropTypes.string.isRequired,
-  }),
-  onRevealTile: PropTypes.func,
 };
