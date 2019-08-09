28695d22120c91ba1c30b6f9da39c2343cd386a9
Rimian Perkins <hello@rimian.com.au>
2019-08-09 17:13:58 +1000

    empty storybook

--- src/components/Tile.stories.js
+++ src/components/Tile.stories.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
 
 import Tile from './Tile';
 
-export const tile = {
-  id: '1',
-  state: 'DEFAULT',
-};
-
-export const actions = {
-  onRevealTile: action('onRevealTile'),
-};
-
 storiesOf('Tile', module)
-  .add('default', () => <Tile tile={{...tile}} {...actions}/>);
+  .add('default', () => <Tile/>);
