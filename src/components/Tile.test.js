658cc692d1419ba1270008c9761716ba9681a7c6
Rimian Perkins <hello@rimian.com.au>
2019-08-10 14:29:10 +1000

    move the test to where it runs

--- src/components/Tile.test.js
+++ src/components/Tile.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
-import Tile from '../Tile';
+import Tile from './Tile';
 
 // automatically unmount and cleanup DOM after the test is finished.
 afterEach(cleanup);
 
-it('tile changes the state after click', () => {
+it('tile is truthy', () => {
   const tile = render(
     <Tile/>,
   );
