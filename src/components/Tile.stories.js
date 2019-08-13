0bee4e4fcbd4a2cfb85201252a287d7b8213b070
Rimian Perkins <hello@rimian.com.au>
2019-08-13 19:22:30 +1000

    a few more tile states

--- src/components/Tile.stories.js
+++ src/components/Tile.stories.js
@@ -4,4 +4,7 @@ import { storiesOf } from '@storybook/react';
 import Tile from './Tile';
 
 storiesOf('Tile', module)
-  .add('default', () => <Tile/>);
+  .add('Default', () => <Tile value="DEFAULT"/>)
+  .add('Blank', () => <Tile value="BLANK"/>)
+  .add('Exploded!', () => <Tile value="EXPLODED"/>)
+  .add('1', () => <Tile value="1"/>);
--- src/components/Tile.test.js
+++ src/components/Tile.test.js
@@ -6,12 +6,14 @@ import Tile from './Tile';
 // automatically unmount and cleanup DOM after the test is finished.
 afterEach(cleanup);
 
-it('tile has a state', () => {
-  const component = render(
-    <Tile id="2" value='DEFAULT'/>,
-  );
-
+it('has a state', () => {
+  const component = render(<Tile id="2" value='DEFAULT'/>);
   const element = getByTestId(document.body, 'tile-2');
-
   expect(element.classList).toContain('DEFAULT');
+});
+
+it('has exploded', () => {
+  const component = render(<Tile id="2" value='EXPLODED'/>);
+  const element = getByTestId(document.body, 'tile-2');
+  expect(element.classList).toContain('EXPLODED');
 });
